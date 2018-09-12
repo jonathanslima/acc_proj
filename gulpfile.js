bd267d21ce7b563bf5fccd040fd0023421e8b1f0
Jonathan <jonathan.da.s.lima@accenture.com>
2018-09-12 16:23:16 -0300

    update all config files

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,23 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
-// Static Server + watching scss/html files
+/* Static Server + watching scss/html files */
 gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+      server: "./"
+  });
 
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("sass/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("sass/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+/* Compile sass into CSS & auto-inject into browsers */
 gulp.task('sass', function() {
-    return gulp.src("sass/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("css/"))
-        .pipe(browserSync.stream());
+  return gulp.src("sass/*.scss")
+    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
