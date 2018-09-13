54d1313053127b4edfba919c330c8cefe4e2961a
jonathanslima <jonathan633.jl@gmail.com>
2018-09-13 02:24:26 -0300

    add ux to form

--- js/login.js
+++ js/login.js
@@ -1,20 +1,36 @@
 $(document).ready(function(){
 	$('#btn-login').on('click', function(e){
 		e.preventDefault();
-		console.log('oioioi');
-		$.ajax({
-	    url: "https://reqres.in/api/login",
-	    type: "POST",
-	    data: {
-			    "email": "peter@klaven",
-			    "password": "cityslicka"
-			},
-	    success: function(response){
-	        console.log(response);
+		var $user = $('#user');
+		var $pass = $('#pass');
 
-	        var token = JSON.stringify(response);
-	        localStorage.setItem('token', token);
-	    }
+		$.ajax({
+			url: "https://reqres.in/api/login",
+			type: "POST",
+			data: {
+			    "email": $user.val(),
+			    "password": $pass.val()
+			},
+			success: function(response){
+				$('.button-outline').addClass('button-spinner');
+				setTimeout(function(){
+					var token = JSON.stringify(response);
+					localStorage.setItem('token', token);
+					$('.button-outline').addClass('button-check');
+				}, 1000);
+
+				setTimeout(function(){
+					window.location.href = '/list';				
+				}, 3000);
+			},
+			error: function(){
+				console.log('falhou');
+				$('.invalid-feedback').fadeIn();
+			},
+			complete: function(){
+				console.log('apareco sempre')
+
+			}
 		});
 	})
 });
