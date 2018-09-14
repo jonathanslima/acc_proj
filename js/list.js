f3ddf6efc814b15fda3e513cc0835e1b349162cc
Jonathan <jonathan.da.s.lima@accenture.com>
2018-09-14 13:31:13 -0300

    iniciado listagem e paginacao

--- js/list.js
+++ js/list.js
@@ -1,19 +1,60 @@
 $(document).ready(function(){
 
-	var $user = $('#user');
-	var $pass = $('#pass');
-	var count = 1;
+	var $user = $('#user'),
+			$pass = $('#pass'),
+			count = 1,
+			$nextList = $('#nextList'),
+			$prevList = $('#prevList'),
+			allUsersData,
+			ul = $('.list-container'),
+			li;
 
-	$.ajax({
-		url: "https://reqres.in/api/users?page=" + count,
-		type: "GET",
+	var mountList = function(count){
+		$.ajax({
+			url: "https://reqres.in/api/users?page=" + count,
+			type: "GET",
 
-		success: function(response){
-			console.log(response);
-		},
-		error: function(){
-			console.log('falhou');
-			$('.invalid-feedback').fadeIn();
+			success: function(response){
+				// console.log(response);
+				// console.log(response.data);
+
+				allUsersData = response.data;
+
+				allUsersData.map(function(user){
+					li = '<li class="list-container-li"><a href="javascript:;" class="list-container-a">' + user.first_name + ' ' + user.last_name + '</a></li>';
+
+					ul.append(li);
+				})
+			},
+			error: function(){
+				console.log('falhou');
+				$('.invalid-feedback').fadeIn();
+			}
+		});
+	}
+
+	mountList();
+
+	$nextList.on('click', function(){
+		ul.empty('.list-container-li');
+		count = count + 1;
+		if(count > 3){
+			console.error(count);
+			$(this).attr('disabled', 'disabled')
 		}
+		console.log(count);
+		mountList(count);
 	});
+
+	$prevList.on('click', function(){
+		ul.empty('.list-container-li');
+		count = count - 1;
+
+		if(count > 1){
+			console.error(count);
+			$(this).attr('disabled', 'disabled')
+		}
+		mountList(count);
+	});
+
 });
